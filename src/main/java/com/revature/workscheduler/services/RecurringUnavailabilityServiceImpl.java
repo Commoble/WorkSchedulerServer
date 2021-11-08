37cf41d293f48b58cfc6bcde0cb8b5c4968a0982
Joseph B <josephbettendorff@yahoo.com>
2021-11-08 17:41:02 -0600

    Fix compiler errors

--- src/main/java/com/revature/workscheduler/services/RecurringUnavailabilityServiceImpl.java
+++ src/main/java/com/revature/workscheduler/services/RecurringUnavailabilityServiceImpl.java
@@ -1,5 +1,6 @@
 package com.revature.workscheduler.services;
 
+import com.revature.workscheduler.models.RecurringUnavailability;
 import com.revature.workscheduler.repositories.RecurringUnavailabilityRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,9 +12,19 @@ public class RecurringUnavailabilityServiceImpl implements RecurringUnavailabili
     private RecurringUnavailabilityRepo rur;
 
     @Override
-    public RecurringUnavailabilityRepo getRecurringUnavailabilityRepo()
+    public RecurringUnavailabilityRepo getRepo()
     {
         return this.rur;
+    }
+
+    /**
+     * @param value A record to get the value of. Must not be null.
+     * @return The ID of the value
+     */
+    @Override
+    public Integer getIDFor(RecurringUnavailability value)
+    {
+        return value.getRecurringUnavailabilityID();
     }
 }
 
