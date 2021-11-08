4aadf95dc40da1b8cd4bce131378878193f0ae41
asalsahli44 <asalsahli44@gmail.com>
2021-11-08 11:06:18 -0600

    added service

--- src/main/java/com/revature/workscheduler/services/RoleServiceImpl.java
+++ src/main/java/com/revature/workscheduler/services/RoleServiceImpl.java
@@ -3,9 +3,11 @@ package com.revature.workscheduler.services;
 import com.revature.workscheduler.models.Role;
 import com.revature.workscheduler.repositories.RoleRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class RoleServiceImpl implements RoleService {
 
     @Autowired
@@ -13,33 +15,12 @@ public class RoleServiceImpl implements RoleService {
 
 
     @Override
-    public Role addRole(Role a) {
-        return ar.save(a);
+    public RoleRepo getRepo() {
+        return ar;
     }
 
     @Override
-    public Role getRole(int id) {
-        return ar.findById(id).get();
-    }
-
-    @Override
-    public List<Role> getAllRoles() {
-        return (List<Role>) ar.findAll();
-    }
-
-    @Override
-    public Role updateRole(Role change) {
-        return ar.save(change);
-    }
-
-    @Override
-    public boolean deleteRole(int id) {
-        try{
-            ar.deleteById(id);
-            return true;
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-            return false;
-        }
+    public Integer getIDFor(Role value) {
+        return value.getRoleID();
     }
 }
