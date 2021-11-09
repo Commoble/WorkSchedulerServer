3c212ccbed193a1d178092b85cf32c83ab8f0535
Joseph B <josephbettendorff@yahoo.com>
2021-11-09 10:01:56 -0600

    Update employee controller test to work with security

--- src/test/java/com/revature/workscheduler/controllers/EmployeeControllerTests.java
+++ src/test/java/com/revature/workscheduler/controllers/EmployeeControllerTests.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -25,10 +26,12 @@ public class EmployeeControllerTests
 	private MockMvc mvc;
 
 	@Test
+	@WithMockUser(username="stevet", password="parseword", roles={"USER","MANAGER"})
 	void getEmployeeByID() throws Exception
 	{
-		Employee expectedEmployee = new Employee(1, "Steve Testingperson","stevet", "parseword", 0);
-		Mockito.when(service.get(1))
+		int id = 1;
+		Employee expectedEmployee = new Employee(id, "Steve Testingperson","stevet", "parseword", 0);
+		Mockito.when(service.get(id))
 			.thenReturn(expectedEmployee);
 		ResultActions actions = mvc.perform(MockMvcRequestBuilders.get("/employees/1"));
 		actions.andExpect(MockMvcResultMatchers.status().isOk());
