499a228f10145e4e65193fb0ef601e95f80161a7
Keven Mitchell <keven.mitchell@revature.net>
2021-11-09 14:47:08 -0500

    ShiftTypeControllerTests.java updated, 100% coverage for ShiftTypeController

--- src/test/java/com/revature/workscheduler/controllers/ShiftTypeControllerTests.java
+++ src/test/java/com/revature/workscheduler/controllers/ShiftTypeControllerTests.java
@@ -1,5 +1,6 @@
 package com.revature.workscheduler.controllers;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.revature.workscheduler.app.WorkschedulerApplication;
 import com.revature.workscheduler.models.ShiftType;
 import com.revature.workscheduler.services.ShiftTypeService;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -44,9 +46,40 @@ public class ShiftTypeControllerTests
     void addShiftType() throws Exception
     {
         ShiftType st = new ShiftType(0,"name", 0,0);
+
         Mockito.when(service.add(st))
                 .thenReturn(st);
-        ResultActions actions = mvc.perform(MockMvcRequestBuilders.post("/shifttypes"));
+        ResultActions actions = mvc.perform(MockMvcRequestBuilders.post("/shifttypes")
+                .contentType("application/json")
+                .content("{\"name\":\"name\",\"startTime\":0,\"endTime\":0}"));
+        actions.andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @WithMockUser(username = "user", password = "pass", roles = {"USER, MANAGER"})
+    @Test
+    void updateShiftType() throws Exception
+    {
+        ShiftType st = new ShiftType(0,"name", 0,0);
+
+        Mockito.when(service.add(new ShiftType()))
+                .thenReturn(st);
+        ResultActions actions = mvc.perform(MockMvcRequestBuilders.put("/shifttypes/0")
+                .contentType("application/json")
+                .content("{}"));
+        actions.andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @WithMockUser(username = "user", password = "pass", roles = {"USER, MANAGER"})
+    @Test
+    void deleteShiftType() throws Exception
+    {
+        ShiftType st = new ShiftType(0,"name", 0,0);
+
+        Mockito.when(service.delete(0))
+                .thenReturn(true);
+        ResultActions actions = mvc.perform(MockMvcRequestBuilders.delete("/shifttypes/0")
+                .contentType("application/json")
+                .content("{}"));
         actions.andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
