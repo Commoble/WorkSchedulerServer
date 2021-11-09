646af4b88f8964d5f2f7bd9e5f66cdbd9a189e53
Keven Mitchell <keven.mitchell@revature.net>
2021-11-09 14:00:57 -0500

    SIKE re-Added ShiftTypeControllerTests.java

---
+++ src/test/java/com/revature/workscheduler/controllers/ShiftTypeControllerTests.java
@@ -0,0 +1,52 @@
+package com.revature.workscheduler.controllers;
+
+import com.revature.workscheduler.app.WorkschedulerApplication;
+import com.revature.workscheduler.models.ShiftType;
+import com.revature.workscheduler.services.ShiftTypeService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AutoConfigureMockMvc
+@SpringBootTest(classes = WorkschedulerApplication.class)
+public class ShiftTypeControllerTests
+{
+    @MockBean
+    private ShiftTypeService service;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @WithMockUser(username = "user", password = "pass", roles = {"USER, MANAGER"})
+    @Test
+    void getShiftTypes() throws Exception
+    {
+        List<ShiftType> shiftTypes = new ArrayList<>();
+        Mockito.when(service.getAll())
+                .thenReturn(shiftTypes);
+        ResultActions actions = mvc.perform(MockMvcRequestBuilders.get("/shifttypes"));
+        actions.andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @WithMockUser(username = "user", password = "pass", roles = {"USER, MANAGER"})
+    @Test
+    void addShiftType() throws Exception
+    {
+        ShiftType st = new ShiftType(0,"name", 0,0);
+        Mockito.when(service.add(st))
+                .thenReturn(st);
+        ResultActions actions = mvc.perform(MockMvcRequestBuilders.post("/shifttypes"));
+        actions.andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
