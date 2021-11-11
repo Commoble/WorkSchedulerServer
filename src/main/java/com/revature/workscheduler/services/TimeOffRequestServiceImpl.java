5b37d94a66b8d212cad0df4e9eff26a05be01532
Keven Mitchell <keven.mitchell@revature.net>
2021-11-10 21:44:20 -0500

    Added Time Off Requests API, repo, service, unit tests

---
+++ src/main/java/com/revature/workscheduler/services/TimeOffRequestServiceImpl.java
@@ -0,0 +1,39 @@
+package com.revature.workscheduler.services;
+
+import com.revature.workscheduler.models.TimeOffRequest;
+import com.revature.workscheduler.repositories.TimeOffRequestRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TimeOffRequestServiceImpl implements TimeOffRequestService
+{
+    @Autowired
+    private TimeOffRequestRepo torr;
+
+    @Override
+    public TimeOffRequestRepo getRepo()
+    {
+        return torr;
+    }
+
+    @Override
+    public Integer getIDFor(TimeOffRequest value)
+    {
+        return value.getTimeOffRequestID();
+    }
+
+    @Override
+    public List<TimeOffRequest> getByEmployeeID(int id)
+    {
+        return torr.findByEmployeeEmployeeID(id);
+    }
+
+    @Override
+    public List<TimeOffRequest> getPendingRequests()
+    {
+        return torr.findByApprovedNull();
+    }
+}
