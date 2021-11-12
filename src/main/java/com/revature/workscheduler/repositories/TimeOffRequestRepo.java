1fbb0a3195dd4c239350f5873ba63632a2142fe3
Joseph B <josephbettendorff@yahoo.com>
2021-11-12 12:01:21 -0600

    Add native sql query for get-undenied-time-off-requests

--- src/main/java/com/revature/workscheduler/repositories/TimeOffRequestRepo.java
+++ src/main/java/com/revature/workscheduler/repositories/TimeOffRequestRepo.java
@@ -1,6 +1,7 @@
 package com.revature.workscheduler.repositories;
 
 import com.revature.workscheduler.models.TimeOffRequest;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -17,5 +18,6 @@ public interface TimeOffRequestRepo extends CrudRepository<TimeOffRequest, Integ
      * @return Gets all time off requests for an employee that are not denied (either pending or approved).
      * Returns an empty list if employee not found.
      */
+    @Query("select t from TimeOffRequest t where t.employee.employeeID = ?1 and t.approved is not false")
     public List<TimeOffRequest> findByEmployeeEmployeeIDAndApprovedNotFalse(int employeeID);
 }
