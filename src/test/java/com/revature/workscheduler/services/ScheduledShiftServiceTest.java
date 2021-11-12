6be6a32bd873db52644e354e5346656e11a8a481
Jada Forde <93663718+jadaforde@users.noreply.github.com>
2021-11-12 17:12:07 -0500

    Service Test

--- src/test/java/com/revature/workscheduler/services/ScheduledShiftServiceTest.java
+++ src/test/java/com/revature/workscheduler/services/ScheduledShiftServiceTest.java
@@ -5,7 +5,6 @@ import com.revature.workscheduler.app.WorkschedulerApplication;
 import com.revature.workscheduler.models.Employee;
 import com.revature.workscheduler.models.ScheduledShift;
 import com.revature.workscheduler.models.ShiftType;
-import com.revature.workscheduler.models.TimeOffRequest;
 import com.revature.workscheduler.repositories.ScheduledShiftRepo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -15,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,19 +28,24 @@ public class ScheduledShiftServiceTest {
     private ScheduledShiftRepo sr;
 
 
-
-   @Test
-    void addScheduledShift()
-    {
+    @Test
+    void addScheduledShift() {
         ScheduledShift MS = new ScheduledShift(0, new ShiftType(), new Employee(), 1);
-        int get = MS.getScheduledShiftID();
+
         Mockito.when(sr.save(MS))
-                .thenReturn(new ScheduledShift(0, new ShiftType(), new Employee(), 1));
-        ScheduledShift give = ss.add(MS);
-        Assertions.assertNotEquals(get, give.getScheduledShiftID());
+                .thenReturn(new ScheduledShift(1, MS.getShiftType(), MS.getEmployee(), MS.getDate()));
+        MS = ss.add(MS);
+
+        Assertions.assertEquals(1, MS.getScheduledShiftID());
     }
 
-
+    @Test
+    void getAllScheduledShifts()
+    {
+        List<ScheduledShift> stList = new LinkedList<>();
+        List<ScheduledShift> stList2 = ss.getAll();
+        Assertions.assertEquals(stList.getClass(), stList2.getClass());
+    }
 
 
 }
